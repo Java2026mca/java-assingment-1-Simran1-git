992b315fb17af843632289bcd1e081ec32bd26c1
Simran1-git <kumarisimran3276@gmail.com>
2026-04-07 01:50:10 +0530

    Completed Spiral Matrix

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -15,6 +15,50 @@ public class Main {
         // 8 9 4
         // 7 6 5
         // Diagonal: 15
+          int[][] matrix = new int[n][n];
+        
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (num <= n * n) {
+            // 1. Move Right
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            // 2. Move Down
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            // 3. Move Left
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = num++;
+            }
+            bottom--;
+
+            // 4. Move Up
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = num++;
+            }
+            left++;
+        }
+
+        // Print Matrix and Calculate Diagonal
+        long diagonalSum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + (j == n - 1 ? "" : " "));
+                if (i == j) {
+                    diagonalSum += matrix[i][j];
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Diagonal: " + diagonalSum);
 
     }
 }
