ae78057060c9d1d9a5487afebd5b95f776393c40
Simran1-git <kumarisimran3276@gmail.com>
2026-04-08 01:24:30 +0530

    Update Main.java

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -16,6 +16,38 @@ public class Main {
         // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
 
         String line = sc.nextLine();
+      String[] tokens = line.split("\\s+");
+        Stack<Integer> stack = new Stack<>();
 
+        for (String token : tokens) {
+            if (token.isEmpty()) continue;
+
+            if (isOperator(token)) {
+                int b = stack.pop(); // Second operand
+                int a = stack.pop(); // First operand
+                stack.push(applyOp(a, b, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        System.out.println(stack.pop());
+        sc.close();
+    }
+
+    private static boolean isOperator(String s) {
+        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
+    }
+
+    private static int applyOp(int a, int b, String op) {
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+            default: return 0;
+        }
     }
 }
+    
+
