4dfd21d993ed851e8cdf59105a9e1fde48426ef9
Simran1-git <kumarisimran3276@gmail.com>
2026-04-07 23:06:18 +0530

    Prime Perfect Program Completed

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -20,6 +20,44 @@ public class Main {
         // Prime
         // Neither
         // Perfect
+         if (!sc.hasNextInt()) return;
+         for (int i = 0; i < n; i++) {
+            if (sc.hasNextInt()) {
+                int num = sc.nextInt();
+                System.out.println(classify(num));
+            }
+        }
+        sc.close();
+    }
 
+    public static String classify(int num) {
+        boolean prime = isPrime(num);
+        boolean perfect = isPerfect(num);
+
+        if (prime && perfect) return "Both";
+        if (prime) return "Prime";
+        if (perfect) return "Perfect";
+        return "Neither";
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static boolean isPerfect(int n) {
+        if (n < 2) return false;
+        int sum = 1;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i * i != n) {
+                    sum += n / i;
+                }
+    }
+}
     }
 }
