be11c551350f08777a08a6ea9c5f2c8546ec691d
Simran1-git <kumarisimran3276@gmail.com>
2026-04-08 01:55:26 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -25,7 +25,7 @@ public class Main {
         int swaps = 0;
         // Standard Bubble Sort: The inner loop must decrease in size
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) { // Important: n-1-i
+            for (int j = 0; j < n - 1; j++) { // Important: n-1-i
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
