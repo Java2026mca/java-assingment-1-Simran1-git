558256f21d94300ec38b3b090e3d3a750b7d67e5
Simran1-git <kumarisimran3276@gmail.com>
2026-04-08 01:06:47 +0530

    bubble sort

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -17,6 +17,30 @@ public class Main {
         // Output:
         // 12 22 25 34 64
         // Swaps: 7
+         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        int swaps = 0;
+        // Manual Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaps++;
+                }
+            }
+        }
+
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + (i == n - 1 ? "" : " "));
+        }
+        System.out.println("\nSwaps: " + swaps);
+        sc.close();
     }
 }
